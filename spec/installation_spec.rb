0cf4808d34746010dc9f7dbe9cce5ae0fde1fe2b
Alexandre Chaussier <alexandre.chaussier@tememlio.com>
2016-03-14 23:49:08 +0100

    Add testing and update test vars

--- spec/installation_spec.rb
+++ spec/installation_spec.rb
@@ -6,19 +6,44 @@ end
 
 describe 'docker Ansible role' do
 
-if ['debian', 'ubuntu'].include?(os[:family])
-    describe 'Specific Debian and Ubuntu family checks' do
+    # Variable definition
+    default_file = ''
+    packages = Array[]
+    process_name = ''
+    service_name = ''
 
-        it 'install role packages' do
-            packages = Array[ 'apt' ]
-
-            packages.each do |pkg_name|
-                expect(package(pkg_name)).to be_installed
-            end
-        end
-
+    if ['debian', 'ubuntu'].include?(os[:family])
+        default_file = '/etc/default/docker'
+        packages = Array[ 'docker-engine' ]
+        process_name = 'docker'
+        service_name = 'docker'
     end
-end
 
+    it 'install role packages' do
+        packages.each do |pkg_name|
+            expect(package(pkg_name)).to be_installed
+        end
+    end
+
+    describe file(default_file) do
+        it { should exist }
+        it { should be_file }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+    end
+
+    describe service(service_name) do
+        it { should be_enabled }
+        it { should be_running }
+    end
+
+    describe process(process_name) do
+        it { should be_running }
+    end
+
+    describe port('8081') do
+        it { should be_listening }
+    end
 end
 
